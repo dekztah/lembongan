07de0426d6de6804baa8175493ea68f745fb3a2d
Peter Csikos <peter.csikos@wavetrip.org>
2020-11-01 15:59:30 +0800

    Easier browserlist

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  presets: [
-    [
-      "@vue/cli-plugin-babel/preset",
-      {
-        corejs: 3,
-        targets: {
-          browsers: ["last 2 versions", "safari > 8", "not ie < 11"]
-        }
-      }
-    ]
-  ]
+  presets: ["@vue/cli-plugin-babel/preset"]
 };
