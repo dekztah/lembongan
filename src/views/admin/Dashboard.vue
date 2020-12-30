c36e764bab6482d670b0c8ce19524c2b6856813a
Peter Csikos <peter.csikos@wavetrip.org>
2020-12-30 22:57:15 +0800

    Admin dashboard based on role

--- src/views/admin/Dashboard.vue
+++ src/views/admin/Dashboard.vue
@@ -1,17 +1,46 @@
 <template lang="pug">
   .dashboard.admin-form
-    .lists
-      .links
-        router-link.button(to="/admin/places-list") Places
-        router-link.button(to="/admin/boats-list") Boats
-        router-link.button(to="/admin/activities-list") Activities
-        router-link.button(to="/admin/services-list") Services
+    template(v-if="isAdmin")
+      .lists
+        .links
+          router-link.button(to="/admin/places-list") Places
+          router-link.button(to="/admin/boats-list") Boats
+          router-link.button(to="/admin/activities-list") Activities
+          router-link.button(to="/admin/services-list") Services
 
-    .add
-      .links
-        router-link.button(to="/admin/plac") + new place
-        router-link.button(to="/admin/boat") + new boat
-        router-link.button(to="/admin/activity") + new activity
-        router-link.button(to="/admin/service") + new service
+      .add
+        .links
+          router-link.button(to="/admin/place") + new place
+          router-link.button(to="/admin/boat") + new boat
+          router-link.button(to="/admin/activity") + new activity
+          router-link.button(to="/admin/service") + new service
+
+    template(v-else)
+      | test card {{ userProfile.ownerOf }}
+      .link(v-for="place in userProfile.ownerOf.places")
+        router-link.button(:to="`/admin/place/${place}`")
 
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+import { db } from "@/firebase/firebase";
+
+export default {
+  computed: {
+    ...mapGetters(["isAdmin", "userProfile"])
+  },
+
+  mounted() {
+    console.log("ff", this.userProfile);
+    for (let type in this.userProfile.ownerOf) {
+      console.log("yy", type);
+    }
+    db.ref("places/18/name")
+      .once("value")
+      .then(snapshot => {
+        console.log("x", snapshot.val());
+      });
+  }
+};
+</script>
