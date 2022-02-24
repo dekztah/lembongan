95f05a106c57ae288e75a7c3841437c89c20d238
Peter Csikos <peter.csikos@wavetrip.org>
2022-02-24 16:48:05 +0100

    Updates...

--- src/views/admin/List.vue
+++ src/views/admin/List.vue
@@ -1,16 +1,17 @@
 <template lang="pug">
-  .admin-list
+.admin-list
     .form
-      input(type="text" v-model="search")
-      | {{ filteredCollection.length }} results
+        input(type="text", v-model="search")
+        | {{ filteredCollection.length }} results
 
-      button.button(@click="addNew") add new {{ itemName }}
+        button.button(@click="addNew") add new {{ itemName }}
 
     .items
-      .list-item(v-if="!loading" v-for="item in filteredCollection")
-        .name {{ item.name }}
-        router-link.button(:to="{ name: $route.meta.item, params: { id: item.key }}") edit
-
+        .list-item(v-if="!loading", v-for="item in filteredCollection")
+            .name {{ item.name }}
+            router-link.button(
+                :to="{ name: $route.meta.item, params: { id: item.key } }"
+            ) edit
 </template>
 
 <script>
@@ -18,36 +19,43 @@ import { mapState } from "vuex";
 import store from "@/store";
 
 export default {
-  data() {
-    return {
-      search: ""
-    };
-  },
-  computed: {
-    ...mapState(["collections", "loading"]),
-    collection() {
-      return this.collections[this.$route.meta.collection];
+    data() {
+        return {
+            search: ""
+        };
     },
-    itemName() {
-      return this.$route.meta.item.toLowerCase();
+    computed: {
+        ...mapState(["collections", "loading"]),
+
+        collection() {
+            return this.collections[this.$route.meta.collection];
+        },
+
+        itemName() {
+            return this.$route.meta.item.toLowerCase();
+        },
+
+        filteredCollection() {
+            return this.collection.filter(item =>
+                this.search
+                    ? item.name
+                          .toLowerCase()
+                          .includes(this.search.toLowerCase())
+                    : true
+            );
+        }
     },
-    filteredCollection() {
-      return this.collection.filter(item =>
-        this.search
-          ? item.name.toLowerCase().includes(this.search.toLowerCase())
-          : true
-      );
+
+    beforeRouteEnter(to, from, next) {
+        store.dispatch("fetchCollection", to.meta.collection).then(() => {
+            next();
+        });
+    },
+
+    methods: {
+        addNew() {
+            this.$router.push(`/admin/${this.itemName}`);
+        }
     }
-  },
-  beforeRouteEnter(to, from, next) {
-    store.dispatch("fetchCollection", to.meta.collection).then(() => {
-      next();
-    });
-  },
-  methods: {
-    addNew() {
-      this.$router.push(`/admin/${this.itemName}`);
-    }
-  }
 };
 </script>
