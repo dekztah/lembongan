2fc71a350fbb54c476b72d228435265fa3cb160c
Peter Csikos <peter.csikos@wavetrip.org>
2022-02-24 16:17:52 +0100

    Updates

--- src/App.vue
+++ src/App.vue
@@ -39,10 +39,10 @@
           .public-footer
             .left
               .last(v-if="lastUpdate") last updated: {{ lastUpdateFormatted }}
-              .contact-me Incorrect data? Contact Martijn on:&nbsp;
-            a.social.wa(href="https://wa.me/6281237940300" target="_blank" rel="noopener") WA
+              .contact-me Incorrect data? Contact me on:&nbsp;
+            a.social.tg(href="https://t.me/dekztah" target="_blank" rel="noopener")
 
-            //- a.kofi(href="https://ko-fi.com/dekztah" target="_blank" rel="noopener")
+            a.kofi(href="https://ko-fi.com/dekztah" target="_blank" rel="noopener")
 
           .admin-footer(v-if="userProfile.name")
             span.username {{ userProfile.name }}
